ed36103926400eee5652ac676facec64a6b02840
awegsche <a.wegscheider7141@gmail.com>
2023-10-16 09:54:34 +0200

    add bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+python3 -m venv venv
+. venv/bin/activate
+
+pip install omc3 matplotlib numpy
+
+cd model
+git clone ssh://git@gitlab.cern.ch:7999/acc-models/acc-models-lhc.git
+cd acc-models-lhc
+git checkout hl16
+cd ../..
